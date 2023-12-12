387226d1733bf9c807813c201c5d4dfe4f929b42
Hai Long <longx3732@gmail.com>
2023-12-12 12:41:38 +0700

    test1

---
+++ javascript.js
@@ -0,0 +1,40 @@
+// // BT1
+// let x1=Number(prompt("Nhập số"))
+// let PI=3.14
+// let y1=4*PI*x1*x1
+// let y11=(4/3)*(PI*x1*x1*x1)
+// console.log(y1)
+// console.log(y11)
+
+// // BT2
+// let x2= Number(prompt("Nhập số"))
+// let y2=(x2%2==0)? "CHAN" : "LE"
+// console.log(y2)
+
+// // BT3
+
+// let day=Number(prompt("Nhập ngày vào đây"))
+// let month=Number(prompt("Nhập tháng vào đây"))
+// let year=Number(prompt("Nhập năm vào đây"))
+// let n= (day+2*month+Math.floor((3*(month+1))/ 5) + year + Math.floor(year / 4)) % 7
+// let dayofweek=(n==0)? "Chủ Nhật" : (n==1)? "Thứ 2" :(n==2)? "Thứ 3" :(n==3)? "Thứ 4" :(n==4)? "Thứ 5" :(n==5)? "Thứ 6" :(n==6)? "Thứ 7": "Ngày không hợp lệ"
+// console.log(dayofweek)
+
+// // BT4
+
+// let x4=prompt("Nhập dữ liệu zô đây nhâ")
+// let y4=x4.slice(-2) === "86" ? "YES" : "NO"
+// console.log(y4)
+
+// // BT5
+// let string="1 + 2 = 4"
+// let newstring=Number(string[0])+Number(string[4])
+// let x5=(Number(string[0])+Number(string[4]) === Number(string[8])) ? "YES" : "NO"
+// console.log(x5)
+
+// // BT6
+// let x6 = "12345612";
+// let y6 = Number(x6.slice(0, 2));
+// let z6 = Number(x6.slice(-2));
+// let n6 = y6 === z6 ? "YES" : "NO";
+// console.log(y6);
